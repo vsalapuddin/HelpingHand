b60e98da1be75187e3bf2cb75ef18ab9c77296d6
Vince Salapuddin <vsalapuddin@ufl.edu>
2020-05-09 14:16:28 -0400

    First landing page

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -2,11 +2,32 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 import './Home.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button,Navbar, Nav, Form, FormControl} from 'react-bootstrap';
+
+
+document.body.style = 'background: #A095BF;';
+
+
 function Home() {
     return (
-    <div>
-    Hello World 1
-    </div>
+        <div>
+    <Navbar bg="light" variant="light">
+    <Navbar.Brand href="#home">HelpingHand</Navbar.Brand>
+        <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Jobs</Nav.Link>
+            <Nav.Link href="#pricing">Log In</Nav.Link>
+        </Nav>
+    </Navbar>
+
+
+            <p className="landingPageTitle">
+            HelpingHand
+            </p>
+        <Button variant="light" className="buttonTitle1" href="#">I am looking for work!</Button> {' '}
+        <Button variant="light" className="buttonTitle2" href="#">I am looking for workers!</Button> {' '}
+        </div>
     );
 }
 
